239a9b140a24dca0c53ea70db1b78034b52666ea
Vedran Milic <vedran.darkangel@gmail.com>
2018-01-14 11:53:11 +0100

    Pagination update

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -10,16 +10,16 @@ export const Pagination = (props) => {
       partialNav = 'none',
       endPage;
 
-    if(props.totalPages < pageSize) {
+    if(props.totalPages <= pageSize) {
       startPage = 1;
       endPage = props.totalPages;
     } else {
       if(props.currentPage <= 3) {
         startPage = 1;
-        endPage = pageSize;
+        endPage = pageSize - 1;
         partialNav = 'right';
       } else if(props.currentPage + 2 >= props.totalPages) {
-        startPage = props.totalPages - 4;
+        startPage = props.totalPages - 3;
         endPage = props.totalPages;
         partialNav = 'left';
       } else {
@@ -28,6 +28,7 @@ export const Pagination = (props) => {
         partialNav = 'both';
       }
     }
+
 
     for(let i = 0; i < endPage+1 - startPage; i++) {
       if(i === 0) {
@@ -67,6 +68,244 @@ export const Pagination = (props) => {
         </ul>
       </div>
     );
+
+    // if(props.totalPages > 6) {
+    //   if(props.currentPage - 5 > 2 && props.currentPage + 5 < props.totalPages - 2) {
+    //     paginationSizeArray.splice(5, props.currentPage + 5);
+    //     return(
+    //       <div className="pagination">
+    //         <ul>
+    //           <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //           <li className="rest">
+    //             <ul>
+    //               <li className={props.currentPage === 1 ? 'active' : ''}><a onClick={() => props.setPage(1)}>1</a></li>
+    //               <li><span>...</span></li>
+    //               {paginationSizeArray.map((page, idx) => {
+    //                 return(
+    //                   <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                     <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                 );
+    //               })}
+    //               <li><span>...</span></li>
+    //               <li className={props.totalPages === props.currentPage ? 'active' : ''}><a  onClick={() => props.setPage(props.totalPages)}>{props.totalPages}</a></li>
+    //             </ul>
+    //           </li>
+    //           <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //         </ul>
+    //       </div>
+    //     );
+    //   } else if(props.currentPage + 5 <= props.totalPages) {
+    //     paginationSizeArray.splice(5, props.currentPage + 5);
+    //     return(
+    //       <div className="pagination">
+    //         <ul>
+    //           <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //           <li className="rest">
+    //             <ul>
+    //               {paginationSizeArray.map((page, idx) => {
+    //                 return(
+    //                   <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                     <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                 );
+    //               })}
+    //               <li><span>...</span></li>
+    //               <li className={props.totalPages === props.currentPage ? 'active' : ''}><a  onClick={() => props.setPage(props.totalPages)}>{props.totalPages}</a></li>
+    //             </ul>
+    //           </li>
+    //           <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //         </ul>
+    //       </div>
+    //     );
+    //   } else if(props.currentPage <= 5) {
+    //     paginationSizeArray.splice(5, props.totalPages.length - 5);
+    //     return(
+    //       <div className="pagination">
+    //         <ul>
+    //           <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //           <li className="rest">
+    //             <ul>
+    //               <li className={props.currentPage === 1 ? 'active' : ''}><a onClick={() => props.setPage(1)}>1</a></li>
+    //               <li><span>...</span></li>
+    //               {paginationSizeArray.map((page, idx) => {
+    //                 return(
+    //                   <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                     <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                 );
+    //               })}
+    //             </ul>
+    //           </li>
+    //           <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //         </ul>
+    //       </div>
+    //     );
+    //   } else {
+    //     return(
+    //       <div className="pagination">
+    //         <ul>
+    //           <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //           <li className="rest">
+    //             <ul>
+    //               {paginationSizeArray.map((page, idx) => {
+    //                 return(
+    //                   <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                     <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                 );
+    //               })}
+    //             </ul>
+    //           </li>
+    //           <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //         </ul>
+    //       </div>
+    //     );
+    //   }
+    // } else {
+    //   return(
+    //     <div className="pagination">
+    //       <ul>
+    //         <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //         <li className="rest">
+    //           <ul>
+    //             {paginationSizeArray.map((page, idx) => {
+    //               return(
+    //                 <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                   <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //               );
+    //             })}
+    //           </ul>
+    //         </li>
+    //         <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //       </ul>
+    //     </div>
+    //   );
+    // }
+
+    // return(
+    //   <div className="pagination">
+    //     <ul>
+    //       <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //       <li className="rest">
+    //         <ul>
+    //           {props.totalPages > 6 }
+    //           {paginationSizeArray.map((page, idx) => {
+    //             //if(idx+1 <= RESULTS_PER_PAGE/2-1) {
+    //             if(props.currentPage <= 2) {
+    //               return(
+    //                 <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                   <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //               );
+    //             }
+    //
+    //             //}
+    //           })}
+    //         </ul>
+    //       </li>
+    //       <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //     </ul>
+    //   </div>
+    // );
+    // if(props.totalPages <= 6) {
+    //   return(
+    //     <div className="pagination">
+    //       <ul>
+    //         <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //         <li className="rest">
+    //           <ul>
+    //             {paginationSizeArray.map((page, idx) => {
+    //               if(idx+1 <= RESULTS_PER_PAGE/2-1) {
+    //                 return(
+    //                   <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                     <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                 );
+    //               }
+    //             })}
+    //           </ul>
+    //         </li>
+    //         <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //       </ul>
+    //     </div>
+    //   );
+    // } else if(props.totalPages > 6 && props.currentPage < 5) {
+    //   return(
+    //     <div className="pagination">
+    //       <ul>
+    //         <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //         <li className="rest">
+    //           <ul>
+    //             {paginationSizeArray.map((page, idx) => {
+    //               if(paginationSizeArray.length !== idx+1) {
+    //                 if(idx+1 <= RESULTS_PER_PAGE/2-1) {
+    //                   return(
+    //                     <li
+    //                       key={idx}
+    //                       className={idx+1 === props.currentPage ? 'active' : ''}
+    //                     ><a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                   );
+    //                 }
+    //               }
+    //             })}
+    //             <li><span>...</span></li>
+    //             <li className={props.totalPages === props.currentPage ? 'active' : ''}><a  onClick={() => props.setPage(props.totalPages)}>{props.totalPages}</a></li>
+    //           </ul>
+    //         </li>
+    //         <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //       </ul>
+    //     </div>
+    //   );
+    // } else if(props.currentPage > props.totalPages -  4) {
+    //   return(
+    //     <div className="pagination">
+    //       <ul>
+    //         <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //         <li className="rest">
+    //           <ul>
+    //             <li className={props.currentPage === 1 ? 'active' : ''}><a onClick={() => props.setPage(1)}>1</a></li>
+    //             <li><span>...</span></li>
+    //             {paginationSizeArray.map((page, idx) => {
+    //               if(idx > 0) {
+    //                 if(idx+1 >= props.totalPages-4+1) {
+    //                   return(
+    //                     <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                       <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //                   );
+    //                 }
+    //               }
+    //             })}
+    //           </ul>
+    //         </li>
+    //         <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //       </ul>
+    //     </div>
+    //   );
+    // } else {
+    //   return(
+    //     <div className="pagination">
+    //       <ul>
+    //         <li className="prev"><a onClick={() => props.setPage(props.currentPage > 0 ? props.currentPage - 1 : 0)}>Prev</a></li>
+    //         <ul className="rest">
+    //           <li className={props.currentPage === 1 ? 'active' : ''}><a onClick={() => props.setPage(1)}>1</a></li>
+    //           <li><span>...</span></li>
+    //           {paginationSizeArray.map((page, idx) => {
+    //             if(
+    //               idx > 0 &&
+    //               paginationSizeArray.length !== idx+1 &&
+    //               idx+1 <= props.currentPage + 1 &&
+    //               props.currentPage - 1 <= idx+1
+    //             ) {
+    //               return(
+    //                 <li key={idx} className={idx+1 === props.currentPage ? 'active' : ''}>
+    //                   <a onClick={() => props.setPage(idx+1)}>{idx+1}</a></li>
+    //               );
+    //             }
+    //           })}
+    //           <li><span>...</span></li>
+    //           <li className={props.totalPages === props.currentPage ? 'active' : ''}>
+    //             <a onClick={() => props.setPage(props.totalPages)}>{props.totalPages}</a></li>
+    //         </ul>
+    //         <li className="next"><a onClick={() => props.setPage(props.currentPage < props.totalPages ? props.currentPage + 1 : props.totalPages)}>Next</a></li>
+    //       </ul>
+    //     </div>
+    //   );
+    //}
   };
 
   return renderPagination();
